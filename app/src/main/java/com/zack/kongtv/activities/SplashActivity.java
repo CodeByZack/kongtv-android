d2e76815a194f5a151a01d41b78716ac86b5d823
zackdk <958059970@qq.com>
2018-07-21 17:45:48 +0800

    splash

---
+++ app/src/main/java/com/zack/kongtv/activities/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.zack.kongtv.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zack.kongtv.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
