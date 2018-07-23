114541aff1f8dc664cb90537028be109e3ed8f8e
zackdk <958059970@qq.com>
2018-07-23 13:44:10 +0800

    投屏

--- app/src/main/java/com/zack/kongtv/activities/PlayMovie/FullScreenActivity.java
+++ app/src/main/java/com/zack/kongtv/activities/PlayMovie/FullScreenActivity.java
@@ -1,18 +1,26 @@
 package com.zack.kongtv.activities.PlayMovie;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.xiao.nicevideoplayer.NiceVideoPlayer;
 import com.xiao.nicevideoplayer.NiceVideoPlayerManager;
 import com.xiao.nicevideoplayer.TxVideoPlayerController;
 import com.zack.kongtv.R;
+import com.zack.kongtv.activities.MainActivity;
 import com.zack.kongtv.view.CustomPlayerControl;
 import com.zackdk.NetWorkChange.NetStateChangeObserver;
 import com.zackdk.NetWorkChange.NetStateChangeReceiver;
@@ -20,15 +28,93 @@ import com.zackdk.NetWorkChange.NetworkType;
 import com.zackdk.Utils.LogUtil;
 import com.zackdk.base.BaseMvpActivity;
 
+import org.fourthline.cling.android.AndroidUpnpService;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+import org.fourthline.cling.model.action.ActionInvocation;
+import org.fourthline.cling.model.message.UpnpResponse;
+import org.fourthline.cling.model.meta.Device;
+import org.fourthline.cling.model.meta.Service;
+import org.fourthline.cling.model.types.ServiceType;
+import org.fourthline.cling.model.types.UDAServiceType;
+import org.fourthline.cling.registry.DefaultRegistryListener;
+import org.fourthline.cling.registry.Registry;
+import org.fourthline.cling.registry.RegistryListener;
+import org.fourthline.cling.support.avtransport.callback.SetAVTransportURI;
+import org.fourthline.cling.support.model.DIDLObject;
+import org.fourthline.cling.support.model.ProtocolInfo;
+import org.fourthline.cling.support.model.Res;
+import org.fourthline.cling.support.model.item.AudioItem;
+import org.fourthline.cling.support.model.item.ImageItem;
+import org.fourthline.cling.support.model.item.VideoItem;
+import org.seamless.util.MimeType;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 
+
 public class FullScreenActivity extends BaseMvpActivity<PlayMoviePresenter> implements IPlayMovieView,NetStateChangeObserver {
+    public static String TEST_URL = "http://disp.titan.mgtv.com/vod.do?fmt=4&pno=1051000&fid=CC3A3E5D49EE9B10DCCC3B3E48734547&file=/c1/2018/06/22_0/CC3A3E5D49EE9B10DCCC3B3E48734547_20180622_1_1_1254.mp4";
+    public static final ServiceType AV_TRANSPORT_SERVICE = new UDAServiceType("AVTransport");
+    private static final String DIDL_LITE_FOOTER = "</DIDL-Lite>";
+    private static final String DIDL_LITE_HEADER = "<?xml version=\"1.0\"?>" +
+            "<DIDL-Lite " + "xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" " +
+            "xmlns:dc=\"http://purl.org/dc/elements/1.1/\" " + "xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\" " +
+            "xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\">";
+
+    public static final int IMAGE_TYPE = 0;
+    public static final int VIDEO_TYPE = 1;
+    public static final int AUDIO_TYPE = 2;
 
 	private Toolbar toolbar;
 	private NiceVideoPlayer mNiceVideoPlayer;
 	private String name,url;
+
+	private MaterialDialog loading;
+	//通过该service 拿到 控制点进行操作
+	private AndroidUpnpService upnpService;
+	//拿到upnpService
+	private ServiceConnection serviceConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			upnpService = (AndroidUpnpService) service;
+			// 添加设备搜索监听
+			upnpService.getRegistry().addListener(registryListener);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			upnpService = null;
+		}
+	};
+	//搜索设备监听器
+	private RegistryListener registryListener = new DefaultRegistryListener(){
+		@Override
+		public void deviceAdded(Registry registry, Device device) {
+			//问题：一个一个回掉回来  如何知道什么时候是最后一个设备回掉？
+			super.deviceAdded(registry, device);
+			if(loading!=null)loading.dismiss();
+			listDevice.add(device);
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					showList();
+				}
+			});
+		}
+
+		@Override
+		public void deviceRemoved(Registry registry, Device device) {
+			super.deviceRemoved(registry, device);
+		}
+	};
+    //设备列表
+    private List<Device> listDevice = new LinkedList<>();
+	private String video_url;
 
 	@Override
 	public int setView() {
@@ -41,13 +127,19 @@ public class FullScreenActivity extends BaseMvpActivity<PlayMoviePresenter> impl
 		name = intent.getStringExtra("name");
 		url = intent.getStringExtra("url");
 		initView();
-		initLogic();
 		presenter.requestData(url);
+		bindService(new Intent(this, AndroidUpnpServiceImpl.class), serviceConnection, Context.BIND_AUTO_CREATE);
 	}
 
-	private void initLogic() {
-	}
-
+    public void onclick(View v) {
+        loading = new MaterialDialog.Builder(this)
+                .content("查找设备中...")
+                .progress(true, 0)
+                .progressIndeterminateStyle(false)
+                .show();
+        // 搜索所有的设备
+        upnpService.getControlPoint().search();
+    }
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -100,6 +192,7 @@ public class FullScreenActivity extends BaseMvpActivity<PlayMoviePresenter> impl
 		@JavascriptInterface
 		public void showSource(final String videoUrl) {
 			LogUtil.d(videoUrl);
+			video_url = videoUrl;
 			mActivity.runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
@@ -159,4 +252,137 @@ public class FullScreenActivity extends BaseMvpActivity<PlayMoviePresenter> impl
 	protected PlayMoviePresenter setPresenter() {
 		return new PlayMoviePresenter();
 	}
+
+    //展示搜索到的设备
+    private void showList() {
+        List<String> list = new LinkedList<>();
+        for (Device d :listDevice) {
+            list.add(d.getDetails().getFriendlyName());
+        }
+        new MaterialDialog.Builder(this)
+                .title("请选择播放设备")
+                .items(list)
+                .itemsCallback(new MaterialDialog.ListCallback() {
+                    @Override
+                    public void onSelection(MaterialDialog dialog, View itemView, int position, CharSequence text) {
+                        chooseDevice(position);
+                    }
+                })
+                .show();
+    }
+
+    private void chooseDevice(int which) {
+        //需要的参数之一
+        final Service avtService = listDevice.get(which).findService(AV_TRANSPORT_SERVICE);
+        //需要的参数之二
+        String metadata = pushMediaToRender(video_url, "id", "name", "0",VIDEO_TYPE);
+
+        upnpService.getControlPoint().execute(new SetAVTransportURI(avtService, video_url,metadata) {
+            @Override
+            public void success(ActionInvocation invocation) {
+                super.success(invocation);
+                showToast("投屏成功！");
+            }
+
+            @Override
+            public void failure(ActionInvocation invocation, UpnpResponse operation, String defaultMsg) {
+                showToast("投屏失败！");
+            }
+        });
+    }
+
+    private String pushMediaToRender(String url, String id, String name, String duration,int ItemType) {
+        long size = 0;
+        long bitrate = 0;
+        Res res = new Res(new MimeType(ProtocolInfo.WILDCARD, ProtocolInfo.WILDCARD), size, url);
+
+        String creator = "unknow";
+        String resolution = "unknow";
+        String metadata = null;
+
+        switch (ItemType){
+            case IMAGE_TYPE:
+                ImageItem imageItem = new ImageItem(id, "0", name, creator, res);
+                metadata = createItemMetadata(imageItem);
+                break;
+            case VIDEO_TYPE:
+                VideoItem videoItem = new VideoItem(id, "0", name, creator, res);
+                metadata = createItemMetadata(videoItem);
+                break;
+            case AUDIO_TYPE:
+                AudioItem audioItem = new AudioItem(id,"0",name,creator,res);
+                metadata = createItemMetadata(audioItem);
+                break;
+        }
+
+        Log.e("tag", "metadata: " + metadata);
+        return metadata;
+    }
+
+    private String createItemMetadata(DIDLObject item) {
+        StringBuilder metadata = new StringBuilder();
+        metadata.append(DIDL_LITE_HEADER);
+
+        metadata.append(String.format("<item id=\"%s\" parentID=\"%s\" restricted=\"%s\">", item.getId(), item.getParentID(), item.isRestricted() ? "1" : "0"));
+
+        metadata.append(String.format("<dc:title>%s</dc:title>", item.getTitle()));
+        String creator = item.getCreator();
+        if (creator != null) {
+            creator = creator.replaceAll("<", "_");
+            creator = creator.replaceAll(">", "_");
+        }
+        metadata.append(String.format("<upnp:artist>%s</upnp:artist>", creator));
+        metadata.append(String.format("<upnp:class>%s</upnp:class>", item.getClazz().getValue()));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date now = new Date();
+        String time = sdf.format(now);
+        metadata.append(String.format("<dc:date>%s</dc:date>", time));
+
+        Res res = item.getFirstResource();
+        if (res != null) {
+            // protocol info
+            String protocolinfo = "";
+            ProtocolInfo pi = res.getProtocolInfo();
+            if (pi != null) {
+                protocolinfo = String.format("protocolInfo=\"%s:%s:%s:%s\"", pi.getProtocol(), pi.getNetwork(), pi.getContentFormatMimeType(), pi
+                        .getAdditionalInfo());
+            }
+            Log.e("tag", "protocolinfo: " + protocolinfo);
+
+            // resolution, extra info, not adding yet
+            String resolution = "";
+            if (res.getResolution() != null && res.getResolution().length() > 0) {
+                resolution = String.format("resolution=\"%s\"", res.getResolution());
+            }
+
+            // duration
+            String duration = "";
+            if (res.getDuration() != null && res.getDuration().length() > 0) {
+                duration = String.format("duration=\"%s\"", res.getDuration());
+            }
+
+            // res begin
+            //            metadata.append(String.format("<res %s>", protocolinfo)); // no resolution & duration yet
+            metadata.append(String.format("<res %s %s %s>", protocolinfo, resolution, duration));
+
+            // url
+            String url = res.getValue();
+            metadata.append(url);
+
+            // res end
+            metadata.append("</res>");
+        }
+        metadata.append("</item>");
+
+        metadata.append(DIDL_LITE_FOOTER);
+
+        return metadata.toString();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(serviceConnection);
+
+    }
 }
