b369fc321173f618d0dbd371c93cac99e7070ee2
zackdk <958059970@qq.com>
2018-09-09 12:10:56 +0800

    更改检测更新地址

--- app/src/main/java/com/zack/kongtv/Const.java
+++ app/src/main/java/com/zack/kongtv/Const.java
@@ -10,7 +10,7 @@ public class Const {
     public static final int History = 5;
     public static final int Collect = 6;
 
-    public static final String CHECK_UPDATE_URL = "http://65.49.209.21/apk/app_update.json";
+    public static final String CHECK_UPDATE_URL = "https://apks-1252514056.cos.ap-chengdu.myqcloud.com/app_update.json";
     public static final String[] AlipayTextArr= {
             "支付宝发红包啦！8月1日-8日还有机会获得最高4888元大红包！长按复制此消息，打开最新版支付宝就能领取！TgolR186Sl",
             "支付宝发红包啦！8月1日-8日还有机会获得最高4888元大红包！长按复制此消息，打开最新版支付宝就能领取！2Tj9iY52Dg"};
